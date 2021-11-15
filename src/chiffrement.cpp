eb6e69da6ebae17cb484809876fde8adf2e81fed
COUNILLE Alexandra <alexandra.fmc@outlook.fr>
2021-11-15 12:23:05 +0100

    Update chiffrement.cpp

--- src/chiffrement.cpp
+++ src/chiffrement.cpp
@@ -11,17 +11,18 @@ int main(int argc, char *argv[])
     char cNomImgLue[250], cNomImgEcrite[250];
     int nH, nW, nTaille, nR, nG, nB;
     int const SEED = 1000;
+    int const TAILLE = 500;
 
     if (argc != 3)
     {
-        printf("Usage: ImageIn.ppm ImageOut.ppm \n");
+        printf("Usage: ImageIn.ppm ImageOut.ppm\n");
         exit(1);
     }
 
     sscanf(argv[1], "%s", cNomImgLue);
     sscanf(argv[2], "%s", cNomImgEcrite);
 
-    OCTET *ImgIn, *ImgOut, *ImgOutY, *ImgOutCr, *ImgOutCb;
+    OCTET *ImgIn, *ImgOut;
 
     lire_nb_lignes_colonnes_image_ppm(cNomImgLue, &nH, &nW);
     nTaille = nH * nW;
@@ -31,27 +32,41 @@ int main(int argc, char *argv[])
     lire_image_ppm(cNomImgLue, ImgIn, nH * nW);
     allocation_tableau(ImgOut, OCTET, nTaille3);
 
-    int const TAILLE = 1000;
-    std::vector<int> tabKey;
+    std::vector<int> tabKey; //{9, 7, 4, 1, 0, 8, 3, 5, 2, 6};
     for(int nb = 0; nb < TAILLE; nb++) {
         tabKey.push_back(nb);
     }
     std::shuffle(tabKey.begin(), tabKey.end(), std::default_random_engine(SEED));
+    // for(int nb = 0; nb < TAILLE - 1; nb++) {
+    //     printf("%d ", tabKey.at(nb));
+    // }
+
+    for(int i = 0; i < nTaille3; i++) {
+        ImgOut[i] = ImgIn[i];
+    }
 
     int id = 0;
     int keyVal;
+    int temp;
 
     for (int i = 0; i < nTaille3; i += 3)
     {
-        keyVal = tabKey[id] * 3;
-        id = id >= SEED - 1 ? 0 : (id + 1);
-        ImgOut[i] = ImgIn[i + keyVal];
-        ImgOut[i + 1] = ImgIn[i + 1 + keyVal];
-        ImgOut[i + 2] = ImgIn[i + 2 + keyVal];
+        keyVal = tabKey.at(id) * 3;
+        if(i + 2 + keyVal < nTaille3) {
+            temp = ImgOut[i];
+            ImgOut[i] = ImgOut[i + keyVal];
+            ImgOut[i + keyVal] = temp;
+            
+            temp = ImgOut[i + 1];
+            ImgOut[i + 1] = ImgOut[i + 1 + keyVal];
+            ImgOut[i + 1 + keyVal] = temp;
 
-        ImgOut[i + keyVal] = ImgIn[i];
-        ImgOut[i + 1 + keyVal] = ImgIn[i + 1];
-        ImgOut[i + 2 + keyVal] = ImgIn[i + 2];
+            temp = ImgOut[i + 2];
+            ImgOut[i + 2] = ImgOut[i + 2 + keyVal];
+            ImgOut[i + 2 + keyVal] = temp;
+        }
+
+        id = id + 1 > TAILLE - 1 ? 0 : id + 1;
     }
 
     ecrire_image_ppm(cNomImgEcrite, ImgOut, nH, nW);
