4100a4ca3e5b198e473c88e47c8c38f14a4c9708
LWK-Malika <64968935+LWK-Malika@users.noreply.github.com>
2021-11-21 12:21:52 +0100

    Update dilatation.c

--- src/dilatation.c
+++ src/dilatation.c
@@ -1,9 +1,5 @@
-// test_couleur.cpp : Seuille une image en niveau de gris
-
 #include <stdio.h>
 #include "image_ppm.h"
-
-
 
 int main(int argc, char* argv[])
 {
@@ -18,7 +14,7 @@ int main(int argc, char* argv[])
 
     sscanf(argv[1], "%s", cNomImgLue);
     sscanf(argv[2], "%s", cNomImgEcrite);
-    sscanf(argv[3], "%d", &X);
+    
 
     OCTET* ImgIn, * ImgOut;
 
@@ -29,8 +25,7 @@ int main(int argc, char* argv[])
     lire_image_pgm(cNomImgLue, ImgIn, nH * nW);
     allocation_tableau(ImgOut, OCTET, nTaille);
 
-    int cpt =0;
-    while(cpt < X){
+ 
         for (int i = 0; i < nH; i++)
             for (int j = 0; j < nW; j++)
             {
@@ -63,8 +58,6 @@ int main(int argc, char* argv[])
                 else
                     ImgOut[i * nW + j] = ImgIn[i * nW + j];
             }
-      cpt++;
-    }
 
             
     
