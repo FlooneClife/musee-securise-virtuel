4438bbb84515dd9555d0ad275789c64a05ef0f26
COUNILLE Alexandra <alexandra.fmc@outlook.fr>
2021-11-24 12:45:44 +0100

    Ajout choix du nombre de bloc

--- src/Image_bloc.cpp
+++ src/Image_bloc.cpp
@@ -13,6 +13,7 @@ using namespace std;
 
 int const SEED = 1000;
 int const TAILLE = 500;
+int nbBloc;
 
 void copie(int nTaille, OCTET* ImgIn, OCTET* ImgOut){
       for (int i=0; i < (nTaille); i++)
@@ -26,122 +27,55 @@ void Bloc(int nH, int nW,OCTET* ImgIn, OCTET* ImgOut){
    int sumR;
    int sumV;
    int sumB;
-// 12 car je fais des blocs de 4x4 pour commencer donc 3 *4 = 12
-    for (int i = 0; i < nH * 3 ; i += 12){
-     
-      for (int j = 0; j < nW * 3; j += 12){ 
+   int cptNbBlock = 0;
+    for (int i = 0; i < nH * 3 ; i += nbBloc * 3){
+      for (int j = 0; j < nW * 3; j += nbBloc * 3){ 
         sumR =0;
         sumV =0;
         sumB =0;
+        for (int k = i ; k < i + (nbBloc*3); k += 3){
+          for (int l = j; l < j + (nbBloc*3); l += 3) {
+            if((l < nW * 3) && (k < nH * 3)) {
+              sumR += ImgIn[k * nW +l];
+              sumV += ImgIn[k * nW +(l + 1)];
+              sumB += ImgIn[k * nW +(l + 2)];
+              cptNbBlock++;
+            }
+          }
+        }
+        sumR /= cptNbBlock;
+        sumV /= cptNbBlock;
+        sumB /= cptNbBlock;
 
-      for (int k = i ; k <= i + 12; k += 3){
-        for (int l = j ; l <= j + 12; l += 3) {
-         sumR += ImgIn[k * nW +l];
-         sumV += ImgIn[k * nW +(l + 1)];
-         sumB += ImgIn[k * nW +(l+ 2)];
-      
-        }}
-
-        for (int k = i ; k <= i + 12; k += 3){
-        for (int l = j ; l <= j + 12; l += 3) {
-            ImgOut[k * nW +l] = (sumR / 24) > 255 ? 255 : sumR / 24 ;
-            ImgOut[k * nW +l + 1] = (sumV / 24) > 255 ? 255 : sumV / 24  ;
-            ImgOut[k * nW +l + 2] = (sumB / 24) > 255 ? 255 : sumB / 24  ;
-      //24 car 2*12
-        }}
-
-
+        for (int k = i ; k < i + (nbBloc*3); k += 3){
+          for (int l = j ; l < j + (nbBloc*3); l += 3) {
+            if((l < nW * 3) && (k < nH * 3)) {
+            ImgOut[k * nW +l] = sumR;
+            ImgOut[k * nW +l + 1] = sumV;
+            ImgOut[k * nW +l + 2] = sumB;
+            }
+          }
+        }
+        cptNbBlock = 0;
       }
-
-
     }
 
 }
-
-void BlocV2(int nH, int nW,OCTET* ImgIn, OCTET* ImgOut){
-
-   int sumR;
-   int sumV;
-   int sumB;
-    for (int i = 0; i < nH * 3  ; i += 48){
-     
-      for (int j = 0; j < nW * 3  ;  j += 48){ 
-        sumR =0;
-        sumV =0;
-        sumB =0;
-
-      for (int k = i ; k < i + 48; k += 3){
-        for (int l = j ; l < j + 48; l += 3) {
-         sumR += ImgIn[k * nW +l];
-         sumV += ImgIn[k * nW +(l + 1)];
-         sumB += ImgIn[k * nW +(l+ 2)];
-      
-        }}
-
-        for (int k = i ; k < i + 48; k += 3){
-        for (int l = j ; l < j + 48; l += 3) {
-            ImgOut[k * nW +l] = (sumR / (48 * 5) ) > 255 ? 255 : sumR / (48 * 5) ;
-            ImgOut[k * nW +l + 1] = (sumV / (48 * 5)) > 255 ? 255 : sumV / (48 * 5)  ;
-            ImgOut[k * nW +l + 2] = (sumB / (48 * 5)) > 255 ? 255 : sumB / (48 * 5)  ;
-        }}
-
-
-      }
-
-
-    }
-
-}
-
-void BlocV3(int nH, int nW,OCTET* ImgIn, OCTET* ImgOut){
-
-   int sumR;
-   int sumV;
-   int sumB;
-    for (int i = 0; i < nH * 3  ; i += 24){
-     
-      for (int j = 0; j < nW * 3  ;  j += 24){ 
-        sumR =0;
-        sumV =0;
-        sumB =0;
-
-      for (int k = i ; k < i + 24; k += 3){
-        for (int l = j ; l < j + 24; l += 3) {
-         sumR += ImgIn[k * nW +l];
-         sumV += ImgIn[k * nW +(l + 1)];
-         sumB += ImgIn[k * nW +(l+ 2)];
-      
-        }}
-
-        for (int k = i ; k < i + 24; k += 3){
-        for (int l = j ; l < j + 24; l += 3) {
-            ImgOut[k * nW +l] = (sumR / (24 * 2.5) ) > 255 ? 255 : sumR / (24 * 2.5) ;
-            ImgOut[k * nW +l + 1] = (sumV / (24 * 2.5)) > 255 ? 255 : sumV / (24 * 2.5)  ;
-            ImgOut[k * nW +l + 2] = (sumB / (24 * 2.5)) > 255 ? 255 : sumB / (24 * 2.5)  ;
-        }}
-
-
-      }
-
-
-    }
-
-}
-
 
 int main(int argc, char *argv[])
 {
     char cNomImgLue[250], cNomImgEcrite[250];
     int nH, nW, nTaille, nR, nG, nB;
 
-    if (argc != 2)
+    if (argc != 4)
     {
-        printf("Usage: ImageIn.ppm \n");
+        printf("Usage: ImageIn.ppm ImageOut.ppm nbBloc\n");
         exit(1);
     }
 
     sscanf(argv[1], "%s", cNomImgLue);
-    //sscanf(argv[2], "%s", cNomImgEcrite);
+    sscanf(argv[2], "%s", cNomImgEcrite);
+    sscanf(argv[3], "%d", &nbBloc);
 
     OCTET *ImgIn, *ImgOut, *ImgOut2,*ImgOut3;
 
@@ -156,24 +90,10 @@ int main(int argc, char *argv[])
     allocation_tableau(ImgOut3, OCTET, nTaille3);
 
 
-  printf("Creation de la nouvelle image 4x4 ...\n");
-  char nomOut[250]= "Bloc4.ppm";
+  printf("Creation de la nouvelle image %dx%d ...\n", nbBloc, nbBloc);
 //  copie(nTaille3, ImgIn,ImgOut);
   Bloc(nH,nW,ImgIn,ImgOut);
-  ecrire_image_ppm(nomOut, ImgOut, nH, nW);
-
- printf("Creation de la nouvelle image 8x8...\n");
-  char nomOut3[250]= "Bloc8.ppm";
-//  copie(nTaille3, ImgIn,ImgOut);
-  BlocV3(nH,nW,ImgIn,ImgOut3);
-  ecrire_image_ppm(nomOut3, ImgOut3, nH, nW);
- 
-  printf("Creation de la nouvelle image 16x16...\n");
-  char nomOut2[250]= "Bloc16.ppm";
-//  copie(nTaille3, ImgIn,ImgOut);
-  BlocV2(nH,nW,ImgIn,ImgOut2);
-  ecrire_image_ppm(nomOut2, ImgOut2, nH, nW);
-
+  ecrire_image_ppm(cNomImgEcrite, ImgOut, nH, nW);
 
     free(ImgIn);
     printf("\nDone!");
